37fe429174c4be93218033e91aebbd30e8b36d78
lost273 <dnk273@gmail.com>
2017-10-21 11:44:11 +0300

    21-10-2017-11-44

--- HelperMethods/Models/Person.cs
+++ HelperMethods/Models/Person.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace HelperMethods.Models {
+    [DisplayName("New Person")]
     public class Person {
+        [HiddenInput(DisplayValue = false)]
         public int PersonId { get; set; }
+        [Display(Name = "First")]
+        [UIHint("MultilineText")]
         public string FirstName { get; set; }
+        [Display(Name = "Last")]
         public string LastName { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
         public Address HomeAddress { get; set; }
+        [Display(Name = "Approved")]
         public bool IsApproved { get; set; }
         public Role Role { get; set; }
     }
