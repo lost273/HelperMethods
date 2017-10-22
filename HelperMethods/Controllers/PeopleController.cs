f4a791ca858ddae804ca897ae3c5996cbe6d1e20
lost273 <dnk273@gmail.com>
2017-10-22 11:38:58 +0300

    22-10-2017-11-38

--- HelperMethods/Controllers/PeopleController.cs
+++ HelperMethods/Controllers/PeopleController.cs
@@ -16,13 +16,24 @@ namespace HelperMethods.Controllers {
         public ActionResult Index() {
             return View();
         }
-        public PartialViewResult GetPeopleData(string selectedRole = "All") {
+        private IEnumerable<Person> GetData(string selectedRole) {
             IEnumerable<Person> data = personData;
             if (selectedRole != "All") {
                 Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
                 data = personData.Where(p => p.Role == selected);
             }
-            return PartialView(data);
+            return data;
+        }
+        public JsonResult GetPeopleDataJson(string selectedRole = "All") {
+            var data = GetData(selectedRole).Select(p => new {
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                Role = Enum.GetName(typeof(Role), p.Role)
+            });
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public PartialViewResult GetPeopleData(string selectedRole = "All") {
+           return PartialView(GetData(selectedRole));
         }
         public ActionResult GetPeople(string selectedRole = "All") {
             return View((object)selectedRole);
